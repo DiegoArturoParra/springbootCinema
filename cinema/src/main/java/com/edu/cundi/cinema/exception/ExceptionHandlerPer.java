8221e571f4062ab91bff450b51de384499d4b5b2
diegoparra <48141200+DiegoArturoParra@users.noreply.github.com>
2021-09-29 10:09:32 -0500

    arreglos

--- cinema/src/main/java/com/edu/cundi/cinema/exception/ExceptionHandlerPer.java
+++ cinema/src/main/java/com/edu/cundi/cinema/exception/ExceptionHandlerPer.java
@@ -10,7 +10,6 @@ import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.http.converter.HttpMessageNotReadableException;
-import org.springframework.transaction.TransactionSystemException;
 import org.springframework.validation.FieldError;
 import org.springframework.web.HttpMediaTypeNotAcceptableException;
 import org.springframework.web.HttpMediaTypeNotSupportedException;
